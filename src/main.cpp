99165b435767678258081e0c05c43d949db9a01e
Seth <seth@localhost.localdomain>
2017-07-14 16:01:30 -0400

    Added instruction map. Still working out the kinks.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,129 @@
 #include <memory>
 #include <cassert>
 #include <algorithm>
+#include <map>
 
 const int MAX_REG = 32;
 typedef uint32_t Instruction;
+
+const Instruction Addi  = 1  << 26;
+const Instruction Addiu = 2  << 26;
+const Instruction Andi  = 3  << 26;
+const Instruction Beq   = 4  << 26;
+const Instruction Bne   = 5  << 26;
+const Instruction Lbu   = 6  << 26;
+const Instruction Lhu   = 7  << 26;
+const Instruction Lui   = 8  << 26;
+const Instruction Lw    = 9  << 26;
+const Instruction Ori   = 10 << 26;
+const Instruction Sb    = 11 << 26;
+const Instruction Sh    = 12 << 26;
+const Instruction Slti  = 13 << 26;
+const Instruction Sltiu = 14 << 26;
+const Instruction Sw    = 15 << 26;
+const Instruction J     = 16 << 26;
+const Instruction Jal   = 17 << 26;
+
+const Instruction Add   = 1;
+const Instruction Addu  = 2;
+const Instruction And   = 3;
+const Instruction Div   = 4;
+const Instruction Divu  = 5;
+const Instruction Jr    = 6;
+const Instruction Mfc0  = 7;
+const Instruction Mfhi  = 8;
+const Instruction Mflo  = 9;
+const Instruction Mult  = 10;
+const Instruction Multu = 11;
+const Instruction Nor   = 12;
+const Instruction Or    = 13;
+const Instruction Sll   = 14;
+const Instruction Slt   = 15;
+const Instruction Sltu  = 16;
+const Instruction Sra   = 17;
+const Instruction Srl   = 18;
+const Instruction Sub   = 19;
+const Instruction Subu  = 20;
+const Instruction Xor   = 21;
+
+const std::map<std::string, Instruction> Map_str_to_type =
+{
+	{"addi", Addi}, {"addiu", Addiu}, {"andi", Andi}, {"beq", Beq},
+	{"bne",  Bne }, {"lbu",   Lbu  }, {"lhu",  Lhu }, {"lui", Lui},
+	{"lw",   Lw  }, {"ori",   Ori  }, {"sb",   Sb  }, {"sh", Sh},
+	{"slti", Slti}, {"sltiu", Sltiu}, {"sw",   Sw  }, {"j", J},
+	{"jal",  Jal }, {"add",   Add  }, {"addu", Addu}, {"and", And},
+	{"div",  Div }, {"divu",  Divu }, {"jr",   Jr  }, {"mfc0", Mfc0},
+	{"mfhi", Mfhi}, {"mflo",  Mflo }, {"mult", Mult}, {"multu", Multu},
+	{"nor",  Nor }, {"or",    Or   }, {"sll",  Sll }, {"slt", Slt},
+	{"sltu", Sltu}, {"sra",   Sra  }, {"srl",  Srl }, {"sub", Sub},
+	{"subu", Subu}, {"xor",   Xor  }
+};
+
+bool j_type(Instruction i)
+{
+	return i == J || i == Jal;
+}
+
+bool i_type(Instruction i)
+{
+	return i & 0xfc000000;
+}
+
+bool r_type(Instruction i)
+{
+	return i & 0x3f;
+}
+
+void lowercase(std::string& s)
+{
+	for (auto it = s.begin(); it != s.end(); ++it)
+	{
+		if (isalpha(*it))
+			*it = tolower(*it);
+	}
+}
+
+Instruction generate(const std::string& s)
+{
+	std::string token = "";
+	Instruction i;
+	auto it = s.begin();
+	while (it != s.end() && *it != '$')
+	{
+		if (isspace(*it))
+		{
+			if (token == "")
+				continue;
+			else
+				break;
+		}
+		token.push_back(*it);
+	}
+	lowercase(token);
+	// May need to check if there is no instruction (eg, next to a label)
+	i = Map_str_to_type.at(token);
+
+	token = "";
+	for (it; it != s.end(), *it != ','; ++it)
+	{
+		if (isspace(*it))
+			continue;
+		token.push_back(*it);
+	}
+	if (token != "")
+	{
+		// Check for register.
+		if (*token.begin() == '$')
+		// Check for label.
+		else if (isalpha(*token.begin()))
+		// Immediate value.
+		else
+			//
+	}
+
+
+}
 
 int opcode(Instruction i)
 {
