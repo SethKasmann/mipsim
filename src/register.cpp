6952dcfe7f103f1381dcf1454856d63ae8588824
Seth <seth@localhost.localdomain>
2017-07-27 16:02:37 -0400

    Consolidated register class.

--- src/register.cpp
+++ src/register.cpp
@@ -1,63 +1,97 @@
 #include "register.h"
 
-int Registers::get_pc() const
+Registers::Registers()
 {
-	return _pc;
+    init();
 }
 
-uint32_t Registers::get_ir() const
+Registers::Registers(std::vector<Label>& labels)
+{
+    init();
+    for (auto it = labels.begin(); it != labels.end(); ++it)
+    {
+        if (it->name == "main")
+        {
+            _pc = it->loc;
+            break;
+        }
+    }
+}
+
+void Registers::init()
+{
+    _pc  = 0;
+    _ir  = 0;
+    _lo  = 0;
+    _hi  = 0;
+    _exit = false;
+    for (int i = 0; i < MAX_REG; ++i)
+        _r[i] = 0;    
+}
+
+Register& Registers::operator[](int i)
+{
+    return i == pc ? _pc : i == lo ? _lo : i == hi ? _hi : _r[i];
+}
+
+bool Registers::get_exit() const
+{
+    return _exit;
+}
+
+Instruction Registers::get_ir() const
 {
     return _ir;
 }
 
-void Registers::set_ir(uint32_t i)
+void Registers::set_exit(bool e)
+{
+    _exit = e;
+}
+
+void Registers::set_ir(Instruction i)
 {
     _ir = i;
 }
-void Registers::operator++(int)
+
+std::ostream& operator<<(std::ostream& o, Registers& r)
 {
-    _pc += 4;
-}
-void Registers::operator=(int i)
-{
-    _pc = i;
-}
-int& Registers::operator[](int i)
-{
-    return i == pc ? _pc
-         : i == lo ? _lo
-         : i == hi ? _hi
-         : _r[i];
-}
-int Registers::opcode() const
-{
-    return (_ir & 0xfc000000) >> 26;
-}
-int Registers::rs() const
-{
-    return (_ir & 0x3e00000) >> 21;
-}
-int Registers::rt() const
-{
-    return (_ir & 0x1f0000) >> 16;
-}
-int Registers::rd() const
-{
-    return (_ir & 0xf800) >> 11;
-}
-int Registers::imm() const
-{
-    return _ir & 0xffff;
-}
-int Registers::funct() const
-{
-    return _ir & 0x3f;
-}
-int Registers::fetch(int reg) const
-{
-	return _r[reg];
-}
-void Registers::write_back(int reg, int val)
-{
-	_r[reg] = val;
+    o << "   PC: " << std::setw(10) << r._pc      << ' '     
+      << "   IR: " << std::setw(10) << r._ir      << '\n'
+      << "$zero: " << std::setw(10) << r._r[zero] << ' '
+      << "  $at: " << std::setw(10) << r._r[at]   << ' '
+      << "  $v0: " << std::setw(10) << r._r[v0]   << ' '
+      << "  $v1: " << std::setw(10) << r._r[v1]   << '\n'
+      << "  $a0: " << std::setw(10) << r._r[a0]   << ' '
+      << "  $a1: " << std::setw(10) << r._r[a1]   << ' '
+      << "  $a2: " << std::setw(10) << r._r[a2]   << ' '
+      << "  $a3: " << std::setw(10) << r._r[a3]   << '\n'
+      << "  $t0: " << std::setw(10) << r._r[t0]   << ' '
+      << "  $t1: " << std::setw(10) << r._r[t1]   << ' '
+      << "  $t2: " << std::setw(10) << r._r[t2]   << ' '
+      << "  $t3: " << std::setw(10) << r._r[t3]   << '\n'
+      << "  $t4: " << std::setw(10) << r._r[t4]   << ' '
+      << "  $t5: " << std::setw(10) << r._r[t5]   << ' '
+      << "  $t6: " << std::setw(10) << r._r[t6]   << ' '
+      << "  $t7: " << std::setw(10) << r._r[t7]   << '\n'
+      << "  $s0: " << std::setw(10) << r._r[s0]   << ' '
+      << "  $s1: " << std::setw(10) << r._r[s1]   << ' '
+      << "  $s2: " << std::setw(10) << r._r[s2]   << ' '
+      << "  $s3: " << std::setw(10) << r._r[s3]   << '\n'
+      << "  $s4: " << std::setw(10) << r._r[s4]   << ' '
+      << "  $s5: " << std::setw(10) << r._r[s5]   << ' '
+      << "  $s6: " << std::setw(10) << r._r[s6]   << ' '
+      << "  $s7: " << std::setw(10) << r._r[s7]   << '\n'
+      << "  $t8: " << std::setw(10) << r._r[t8]   << ' '
+      << "  $t9: " << std::setw(10) << r._r[t9]   << ' '
+      << "  $k0: " << std::setw(10) << r._r[k0]   << ' '
+      << "  $k1: " << std::setw(10) << r._r[k1]   << '\n'
+      << "  $gp: " << std::setw(10) << r._r[gp]   << ' '
+      << "  $sp: " << std::setw(10) << r._r[sp]   << ' '
+      << "  $fp: " << std::setw(10) << r._r[fp]   << ' '
+      << "  $ra: " << std::setw(10) << r._r[ra]   << '\n'
+      << "  $lo: " << std::setw(10) << r._lo      << ' '
+      << "  $hi: " << std::setw(10) << r._hi      << '\n';
+
+    return o;
 }
--- src/register.h
+++ src/register.h
@@ -3,10 +3,13 @@
 
 #include "label.h"
 #include <string>
+#include <iostream>
+#include <iomanip>
 
-const int MAX_REG = 32;
 
 typedef int32_t Register;
+typedef uint32_t Instruction;
+const int MAX_REG = 32;
 
 enum RegisterName
 {
@@ -18,67 +21,33 @@ enum RegisterName
     s4,   s5,   s6,   s7,
     t8,   t9,   k0,   k1,
     gp,   sp,   fp,   ra, 
-    pc,   lo,   hi
+    pc,   ir,   lo,   hi
 };
 
 class Registers
 {
 public:
-    Registers() : _pc(0), _ir(0), _lo(0), _hi(0)
-    {
-        for (int i = 0; i < MAX_REG; ++i)
-        {
-            _r[i] = 0;
-        }
-    }
-    Registers(std::vector<Label>& labels) : Registers()
-    {
-        for (auto it = labels.begin(); it != labels.end(); ++it)
-        {
-            if (it->name == "main")
-            {
-                _pc = it->loc;
-                break;
-            }
-        }
-    }
-    int get_pc() const;
-    uint32_t get_ir() const;
-    int opcode() const;
-    int rs() const;
-    int rt() const;
-    int rd() const;
-    int funct() const;
-    int imm() const;
-    void set_ir(uint32_t i);
-    void operator++(int);
-    void operator=(int i);
-    int& operator[](int i);
-    int fetch(int reg) const;
-    void write_back(int reg, int val);
+    Registers();
+    Registers(std::vector<Label>& labels);
+    void init();
+    bool get_exit() const;
+    Instruction get_ir() const;
+    void set_exit(bool e);
+    void set_ir(Instruction i) ;
+    Register& operator[](int i);
+    friend std::ostream& operator<<(std::ostream& o, Registers& r);
 private:
-    int _r[MAX_REG];
-    int _pc;
-    int _lo;
-    int _hi;
-    uint32_t _ir;
+    Register _r[MAX_REG];
+    Register _pc;
+    Register _lo;
+    Register _hi;
+    Instruction _ir;
+    bool _exit;
 };
 
 inline bool operator==(int i, RegisterName rn)
 {
     return static_cast<RegisterName>(i) == rn;
 }
-
-/*
-int shift(Instruction i)
-{
-    return (i & 0x7c0) >> 6;
-}
-
-int jaddress(Instruction i)
-{
-    return i & 0x3ffffff;
-}
-*/
 
 #endif
