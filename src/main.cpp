10d97c4d537c981f3baf2c2d840c5a2b41bfc166
Seth <seth@localhost.localdomain>
2017-07-31 15:42:19 -0400

    Moved code from main into mips.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,31 @@
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <memory>
-#include <cassert>
-#include <algorithm>
-#include <map>
-#include "instruction.h"
-#include "misc.h"
 #include "register.h"
-#include "label.h"
 #include "memory.h"
 #include "decode.h"
-#include "encode.h"
+#include "mips.h"
 
 int main()
 {
-	Alu alu[128] = { _null };
-	alu[12] = _syscall;
-	alu[16] = _mfhi;
-	alu[27] = _div;
-	alu[32] = _add;
-	alu[36] = _and;
-	alu[65] = _bltz;
-	alu[66] = _j;
-	alu[68] = _beq;
-	alu[69] = _bne;
-	alu[72] = _addi;
-	alu[96] = _lb;
-	alu[104] = _sb;
+    std::string file;
+    mips::intro();
 
-    std::unordered_map<std::string, int> label;
-    Memory mem;
-    Decoder d;
-    std::vector<Label> labels;
-    init_data(mem, labels);
-    encode_text_labels(mem.size(), labels);
-    //init_text_labels(mem, labels);
-    encode_instructions(mem, labels);
-    //init_instructions(mem, labels);
-    Registers regs(labels);
-
-    while (!regs.get_exit())
+    while (mips::select(file))
     {
-    	regs.set_ir(mem.fetch<Instruction>(regs[pc]));
-    	regs[pc] += 4;
-    	d.decode(regs.get_ir());
-    	alu[d.fi()](regs, d, mem);
-    	//std::cout << regs;
-    	//int z;
-    	//std::cin >> z;
-    }
+        Memory    mem;
+        Decoder   dec;
+        Registers regs;
+
+        mips::encode(file, mem, regs);
+
+        while (mips::program(regs))
+        {
+            mips::fetch(mem, regs);
+            mips::update(regs);
+            mips::decode(regs, dec);
+            mips::execute(mem, regs, dec);
+        }
+    } 
 
     return 0;
 }
