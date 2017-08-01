b6badfec5405fbb14c596f2365503e4815e7faf8
Seth <seth@localhost.localdomain>
2017-08-01 14:06:14 -0400

    Probe and Size functions from header to cpp.

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,1 +1,17 @@
-//#include "memory.h"
+#include "memory.h"
+
+bool Memory::probe(int i) const
+{
+	int bytes = 3;
+	for (auto it = _mem.begin() + i; it != _mem.end(); ++it, --bytes)
+	{
+		if (bytes == 0)
+			return true;
+	}
+	return false;
+}
+
+size_t Memory::size() const
+{
+	return _mem.size();	
+}
--- src/memory.h
+++ src/memory.h
@@ -15,32 +15,18 @@ public:
 	template<typename T> void push(T t);
 	template<typename T> void store(T t, int i);
 	template<typename T> T fetch(int i);
-	size_t size() const
-	{
-		return _mem.size();	
-	}
+	size_t size() const;
 	bool probe(int i) const;
 private:
 	std::vector<Byte> _mem;
 };
-
-inline bool Memory::probe(int i) const
-{
-	int bytes = 3;
-	for (auto it = _mem.begin() + i; it != _mem.end(); ++it, --bytes)
-	{
-		if (bytes == 0)
-			return true;
-	}
-	return false;
-}
 
 template<typename T>
 inline T Memory::fetch(int i)
 {
 	if (probe(i))
 	{
-		return  static_cast<T>(_mem[i    ])
+		return  static_cast<T>(_mem[i    ]       )
 		     | (static_cast<T>(_mem[i + 1]) << 8 )
 		     | (static_cast<T>(_mem[i + 2]) << 16)
 		     | (static_cast<T>(_mem[i + 3]) << 24);
@@ -62,7 +48,7 @@ inline void Memory::store(T t, int i)
 {
 	if (probe(i))
 	{
-		_mem[i    ] = static_cast<Byte>(t & 0x000000ff        );
+		_mem[i    ] = static_cast<Byte>( t & 0x000000ff       );
 		_mem[i + 1] = static_cast<Byte>((t & 0x0000ff00) >> 8 );
 		_mem[i + 2] = static_cast<Byte>((t & 0x00ff0000) >> 16);
 		_mem[i + 3] = static_cast<Byte>((t & 0xff000000) >> 24);
@@ -82,7 +68,7 @@ inline void Memory::store<Byte>(Byte b, int i)
 template<typename T>
 inline void Memory::push(T t)
 {
-	_mem.push_back(static_cast<Byte>(t & 0x000000ff      ));
+	_mem.push_back(static_cast<Byte>( t & 0x000000ff       ));
 	_mem.push_back(static_cast<Byte>((t & 0x0000ff00) >> 8 ));
 	_mem.push_back(static_cast<Byte>((t & 0x00ff0000) >> 16));
 	_mem.push_back(static_cast<Byte>((t & 0xff000000) >> 24));
@@ -93,6 +79,5 @@ inline void Memory::push<Byte>(Byte b)
 {
 	_mem.push_back(b);
 }
-
 
 #endif
