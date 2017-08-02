7df09777f2a6a87eb6b8b5627a2075e71d40d6c8
Seth <seth@localhost.localdomain>
2017-08-02 16:41:02 -0400

    Inlined misc functions. TODO: add cpp file.

--- src/misc.h
+++ src/misc.h
@@ -6,14 +6,14 @@
 #include <cassert>
 
 template<class It, class X>
-void safe_increment(It& it, const X& x, int n)
+inline void safe_increment(It& it, const X& x, int n)
 {
     if (std::distance(it, x.end()) >= n)
         std::advance(it, n);
 }
 
 /* Converts a string to all lowercase characters */
-void lowercase(std::string& s)
+inline void lowercase(std::string& s)
 {
     for (auto it = s.begin(); it != s.end(); ++it)
     {
@@ -23,27 +23,27 @@ void lowercase(std::string& s)
 }
 
 /* Returns the correct escape character. Assumes c follows a '\' */
-char get_escape(char c)
+inline char get_escape(char c)
 {
     assert(c == 'n' || c == 't');
     return c == 'n' ? '\n' : '\t';
 }
 
 /* Removes the comments from a line of source code */
-void remove_comments(std::string& line)
+inline void remove_comments(std::string& line)
 {
     line = std::string(line.begin(), std::find(line.begin(), line.end(), '#'));
 }
 
 /* Checks to see if a line contains only whitespace */
-bool is_whitespace(const std::string& line)
+inline bool is_whitespace(const std::string& line)
 {
     return line.find_first_not_of("\t\n ") == std::string::npos;
 }
 
 /* Returns the first character from a line of source code that is not
    whitespace */
-char first_char(const std::string& line)
+inline char first_char(const std::string& line)
 {
     for (auto it = line.begin(); it != line.end(); ++it)
     {
