8c2a8634befb9dc3580794d0ea2e43b36d7b025d
Seth <seth@localhost.localdomain>
2017-08-02 13:10:48 -0400

    Consolidated template specilizations. Moved the template implementations to the cpp file. Also added size_of functions and specializations for all types.

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,24 +1,114 @@
 #include "memory.h"
 
-bool Memory::probe(int i) const
+/* A Mips instruction is 4 bytes. */
+template<> size_t size_of<Instruction>()
 {
-	int bytes = 3;
-	for (auto it = _mem.begin() + i; it != _mem.end(); ++it, --bytes)
-	{
-		if (bytes == 0)
-			return true;
-	}
-	return false;
+	return static_cast<size_t>(4);
 }
 
+/* A Mips word is 4 bytes. */
+template<> size_t size_of<Word>()
+{
+	return static_cast<size_t>(4);
+}
+
+/* A Mips halfword is 2 bytes. */
+template<> size_t size_of<HalfWord>()
+{
+	return static_cast<size_t>(2);
+}
+
+/* A Mips byte is 1 byte. */
+template<> size_t size_of<Byte>()
+{
+	return static_cast<size_t>(1);
+}
+
+/* Returns the size of memory vector. */
 size_t Memory::size() const
 {
 	return _mem.size();	
 }
 
+/* Sets the stack variable to the first index of the stack and adds the stack
+   on to the memory vector. */
 void Memory::init_stack()
 {
 	_stack = _mem.size();
 	std::vector<Byte> stack(Stack_size);
 	_mem.insert(_mem.end(), stack.begin(), stack.end());
 }
+
+/* Checks to see if a fetch or store routine will cross the stack index
+   variable or is greater than the size of the memory vector. If so, 
+   throw an out_of_range exception. */
+template<typename T>
+void Memory::probe(int first) const
+{
+	int last = first + size_of<T>() - 1;
+	if ((first <= _stack && last >= _stack) || last >= _mem.size())
+		throw std::out_of_range("stack overflow occurred.");
+}
+
+/* Explicit instantiation */
+template void Memory::probe<Instruction>(int) const;
+template void Memory::probe<Word>(int) const;
+template void Memory::probe<HalfWord>(int) const;
+template void Memory::probe<Byte>(int) const;
+
+/* Fetches and returns a value from the memory vector. */
+template<typename T>
+inline T Memory::fetch(int index) const
+{
+	probe<T>(index);
+	T ret = static_cast<T>(0);
+
+	for (int i = 0; i < size_of<T>(); ++i)
+		ret |= static_cast<T>(_mem[index + i]) << (i * 8);
+
+	return ret;
+}
+
+/* Explicit instantiation */
+template Instruction Memory::fetch<Instruction>(int) const;
+template Word Memory::fetch<Word>(int) const;
+template HalfWord Memory::fetch<HalfWord>(int) const;
+template Byte Memory::fetch<Byte>(int) const;
+ 
+/* Stores a value to the memory vector. */
+template<typename T>
+inline void Memory::store(T t, int index)
+{
+	static const T mask = static_cast<T>(0xff);
+	probe<T>(index);
+	for (size_t i = 0; i < size_of<T>(); ++i)
+	{
+		int shift = i * Octet_size;
+		_mem[index + i] = static_cast<Byte>((t & mask << shift) >> shift);
+	}
+}
+
+/* Explicit instantiation */
+template void Memory::store<Instruction>(Instruction, int);
+template void Memory::store<Word>(Word, int);
+template void Memory::store<HalfWord>(HalfWord, int);
+template void Memory::store<Byte>(Byte, int);
+ 
+/* Pushes a value to the memory vector, increasing it's size. Only used during
+   the encoding routine. */
+template<typename T>
+inline void Memory::push(T t)
+{
+	static const T mask = static_cast<T>(0xff);
+	for (size_t i = 0; i < size_of<T>(); ++i)
+	{
+		int shift = i * Octet_size;
+		_mem.push_back(static_cast<Byte>((t & mask << shift) >> shift));
+	}
+}
+
+/* Explicit instantiation */
+template void Memory::push<Instruction>(Instruction);
+template void Memory::push<Word>(Word);
+template void Memory::push<HalfWord>(HalfWord);
+template void Memory::push<Byte>(Byte);
--- src/memory.h
+++ src/memory.h
@@ -3,86 +3,37 @@
 
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 
 typedef unsigned char Byte;
 typedef uint32_t Instruction;
 typedef int32_t Word;
 typedef int16_t HalfWord;
 
-// Stack size of 1MB;
-const int Stack_size = 1000 * 1000;
+/* Stack size of 1MB */
+const size_t Stack_size = 1000 * 1000;
+const size_t Octet_size = 8;
+
+/* Returns the size in bytes of a native Mips type while avoiding the
+   ambiguity of sizeof */
+template<typename T> size_t size_of();
+template<> size_t size_of<Instruction>();
+template<> size_t size_of<Word>();
+template<> size_t size_of<HalfWord>();
+template<> size_t size_of<Byte>();
 
 class Memory
 {
 public:
 	void init_stack();
+	size_t size() const;
 	template<typename T> void push(T t);
 	template<typename T> void store(T t, int i);
-	template<typename T> T fetch(int i);
-	size_t size() const;
-	bool probe(int i) const;
+	template<typename T> T fetch(int i) const;
+	template<typename T> void probe(int first) const;
 private:
 	std::vector<Byte> _mem;
 	int _stack;
 };
-
-template<typename T>
-inline T Memory::fetch(int i)
-{
-	if (probe(i))
-	{
-		return  static_cast<T>(_mem[i    ]       )
-		     | (static_cast<T>(_mem[i + 1]) << 8 )
-		     | (static_cast<T>(_mem[i + 2]) << 16)
-		     | (static_cast<T>(_mem[i + 3]) << 24);
-	}
-	else
-	{
-		std::cout << "Error: Not enough memory avaliable to fetch." << std::endl;
-	}
-}
-
-template<>
-inline Byte Memory::fetch<Byte>(int i)
-{
-	return _mem[i];
-}
-
-template<typename T>
-inline void Memory::store(T t, int i)
-{
-	if (probe(i))
-	{
-		_mem[i    ] = static_cast<Byte>( t & 0x000000ff       );
-		_mem[i + 1] = static_cast<Byte>((t & 0x0000ff00) >> 8 );
-		_mem[i + 2] = static_cast<Byte>((t & 0x00ff0000) >> 16);
-		_mem[i + 3] = static_cast<Byte>((t & 0xff000000) >> 24);
-	}
-	else
-	{
-		std::cout << "Error: Not enough memory avaliable to store." << std::endl;
-	}
-}
-
-template<>
-inline void Memory::store<Byte>(Byte b, int i)
-{
-	_mem[i] = b;
-}
-
-template<typename T>
-inline void Memory::push(T t)
-{
-	_mem.push_back(static_cast<Byte>( t & 0x000000ff       ));
-	_mem.push_back(static_cast<Byte>((t & 0x0000ff00) >> 8 ));
-	_mem.push_back(static_cast<Byte>((t & 0x00ff0000) >> 16));
-	_mem.push_back(static_cast<Byte>((t & 0xff000000) >> 24));
-}
-
-template<>
-inline void Memory::push<Byte>(Byte b)
-{
-	_mem.push_back(b);
-}
 
 #endif
